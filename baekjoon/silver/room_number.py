85cef5d280245fbc637184591206354b9aa1786e
Sunny <thstkd92@gmail.com>
2020-10-26 23:10:55 +0900

    백준 알고리즘 - 1475 방번호

---
+++ baekjoon/silver/room_number.py
@@ -0,0 +1,13 @@
+# 1475
+import math
+
+room_num = list(map(int, str(input())))
+num_set = [0] * 9
+print(room_num)
+for i in room_num:
+    if i == 6 or i == 9:
+        num_set[5] += 0.5
+    else:
+        num_set[i] += 1
+
+print(math.ceil(max(num_set)))
