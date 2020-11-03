cc8931ee15c4d9fdb7f4405cd7e0a40ff146d330
Sunny <thstkd92@gmail.com>
2020-11-03 15:53:12 +0900

    파이썬 알고리즘 인터뷰 12. 그래프 - 그래프의 이론 (DFS, BFS)

---
+++ python-interview/chapter12/graph.py
@@ -0,0 +1,51 @@
+graph = {
+    1: [2, 3, 4],
+    2: [5],
+    3: [5],
+    4: [],
+    5: [6, 7],
+    6: [],
+    7: [3]
+}
+
+
+# 깊이 우선 탐색(DFS)
+def recursive_dfs(v, discovered=[]):
+    discovered.append(v)
+    for w in graph[v]:
+        if w not in discovered:
+            discovered = recursive_dfs(w, discovered)
+    return discovered
+
+
+def iterative_dfs(start_v):
+    discovered = []
+    stack = [start_v]
+    while stack:
+        v = stack.pop()
+        if v not in discovered:
+            discovered.append(v)
+            for w in graph[v]:
+                stack.append(w)
+    return discovered
+
+
+print(recursive_dfs(1))
+print(iterative_dfs(1))
+
+
+# 너비 우선 탐색(BFS)
+def iterative_bfs(start_v):
+    discovered = [start_v]
+    queue = [start_v]
+    while queue:
+        v = queue.pop(0)
+        for w in graph[v]:
+            if w not in discovered:
+                discovered.append(w)
+                queue.append(w)
+    return discovered
+
+
+print("BFS")
+print(iterative_bfs(1))
