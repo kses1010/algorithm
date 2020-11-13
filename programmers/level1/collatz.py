c520cd22555d01ece42b3ce56a15e3252ad29b10
Sunny <thstkd92@gmail.com>
2020-11-13 16:43:20 +0900

    프로그래머스 레벨1 - 콜라츠 추측

---
+++ programmers/level1/collatz.py
@@ -0,0 +1,20 @@
+# 콜라츠 추측
+
+def solution(num):
+    count = 0
+    while num != 1:
+        if num % 2 == 0:
+            num //= 2
+            count += 1
+        else:
+            num = (num * 3) + 1
+            count += 1
+        if count > 500:
+            count = -1
+            break
+    return count
+
+
+print(solution(6))
+print(solution(16))
+print(solution(626331))
