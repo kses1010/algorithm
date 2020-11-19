348c7472b84a76c5ade434e65b398b05d05ef505
Sunny <thstkd92@gmail.com>
2020-11-19 21:28:42 +0900

    나동빈 알고리즘 - DFS/BFS (구현하기)
    DFS/BFS 둘 다 방문을 확인하는 이차원 또는 1차원 배열을 만들면 좋다.
    - DFS: 재귀로 푸는 문제. 재귀 이해할 땐 수학의 점화식을 생각하면 이해하기 쉽다.
    
    - BFS: 큐에 담아서 꺼내어 확인하고 넘기는 식으로 한다.

---
+++ nadongbin/chapter5-dfs_bfs/dfs_practice.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = 1
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = 1
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력하기
+        v = queue.popleft()
+        print(v, end=' ')
+        # 아직 방문하지 않은 인접한 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = 1
+
+
+graph1 = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited1 = [0] * 9
+visited2 = [0] * 9
+dfs(graph1, 1, visited1)
+print()
+bfs(graph1, 1, visited2)
