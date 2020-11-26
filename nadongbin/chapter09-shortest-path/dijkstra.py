9a89f053b9964428beaa1ce4afaea47a8127d8bd
Sunny <thstkd92@gmail.com>
2020-11-26 12:12:50 +0900

    나동빈 알고리즘 - 09. 최단 경로 - 다익스트라 알고리즘
    - 좀 더 알기 쉽게 (거리, 노드) -> (노드, 거리) 로 변경.

--- nadongbin/chapter09-shortest-path/dijkstra.py
+++ nadongbin/chapter09-shortest-path/dijkstra.py
@@ -61,22 +61,22 @@ def heap_dijkstra(n, start, graph):
     answer = []
     q = []
     # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
-    heapq.heappush(q, (0, start))
+    heapq.heappush(q, (start, 0))
     distance[start] = 0
     # 큐가 비어있지 않다면
     while q:
         # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-        dist, now_node = heapq.heappop(q)
+        now_node, dist = heapq.heappop(q)
         # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
         if distance[now_node] < dist:  # 노드에서 꺼낸 값이 테이블의 있는 값보다 크다면 처리된 것으로 처리(무시)
             continue
         # 현재 노드와 연결된 다른 인접한 노드들을 확인
         for i in graph[now_node]:
             cost = dist + i[1]  # i[1]은 노드의 거리값을 의미
-            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우 거리테이블 갱신
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+                heapq.heappush(q, (i[0], cost))
 
     # 모든 노드로 가기 위한 최단 거리를 출력
     for i in range(1, n + 1):
@@ -99,3 +99,4 @@ array = [[],
          []]
 # print(solution(n1, m1, start1))
 print(heap_dijkstra(n1, start1, array))
+# [0, 2, 3, 1, 2, 4]
