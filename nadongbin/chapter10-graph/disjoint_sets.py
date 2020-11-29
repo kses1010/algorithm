b025ca6ac08f054dc640e88ff474bea7ff10195b
Sunny <thstkd92@gmail.com>
2020-11-29 19:34:27 +0900

    나동빈 알고리즘 - 10. 그래프 이론 - 서로소 집합
    - 경로 압축 기법을 사용하면 더욱 줄어든다. O(V + M(1 + log2-m/vV)
    - 노드의 개수가 1000개 연산이 100만번일 경우 계산하면 약 1000만 번 가량의 연산이 필요하다.

--- nadongbin/chapter10-graph/disjoint_sets.py
+++ nadongbin/chapter10-graph/disjoint_sets.py
@@ -6,6 +6,14 @@ def find_parent(parent, x):
     return x
 
 
+# 특정 원소가 속한 집합을 찾기 (경로 압축)
+def find_parent_pc(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        return find_parent_pc(parent, parent[x])
+    return parent[x]
+
+
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
