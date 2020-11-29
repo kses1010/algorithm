4115512083b664e5e88a42cff01fc064e221a2dd
Sunny <thstkd92@gmail.com>
2020-11-29 19:27:35 +0900

    나동빈 알고리즘 - 10. 그래프 이론 - 서로소 집합
    - 서로소 집합 알고리즘: 트리 구조로 이루어져 있으며 시간 복잡도는 노드의 개수 V, find or union 연산의 개수 M 으로 O(VM)이다.

---
+++ nadongbin/chapter10-graph/disjoint_sets.py
@@ -0,0 +1,48 @@
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+# 노드의 개수와 간선의 개수
+v, e = 6, 4
+# 부모 테이블 초기화
+parent1 = [0] * (v + 1)
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, v + 1):
+    parent1[i] = i
+
+
+# union 연산 리스트
+union_list = [[1, 4],
+              [2, 3],
+              [2, 4],
+              [5, 6]]
+# union 연산을 각각 수행
+for i in union_list:
+    union_parent(parent1, i[0], i[1])
+
+# 각 원소가 속한 집합 출력
+print('각 원소가 속한 집합: ', end='')
+for i in range(1, v + 1):
+    print(find_parent(parent1, i), end=', ')
+
+print()
+
+# 부모 테이블 내용 출력
+print('부모 테이블: ', end='')
+for i in range(1, v + 1):
+    print(parent1[i], end=', ')
