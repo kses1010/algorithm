500b1bb1da66b8af5295240aa536a9428759f0dd
Sunny <thstkd92@gmail.com>
2020-12-12 22:36:40 +0900

    소수 찾기 - 에라토스테네스의 체 알기 쉽게 변경

--- programmers/level1/find_prime_number.py
+++ programmers/level1/find_prime_number.py
@@ -6,8 +6,10 @@ def solution(n):
     m = int(n ** 0.5)
     for i in range(2, m + 1):
         if sieve[i]:
-            for j in range(2 * i, n + 1, i):
-                sieve[j] = False
+            j = 2
+            while i * j <= n:
+                sieve[i * j] = False
+                j += 1
 
     return len([i for i in range(2, n + 1) if sieve[i]])
 
