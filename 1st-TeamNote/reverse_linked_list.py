29b54643d82a49a527a0a9e67861bd542a67a8b9
Sunny <thstkd92@gmail.com>
2021-01-06 15:25:57 +0900

    팀노트 추가 - 구간별 역순 연결리스트 추가

--- 1st-TeamNote/reverse_linked_list.py
+++ 1st-TeamNote/reverse_linked_list.py
@@ -4,6 +4,7 @@ class ListNode:
         self.next = next
 
 
+# 역순 연결리스트
 def reverse_list(head: ListNode) -> ListNode:
     node, prev = head, None
 
@@ -12,3 +13,24 @@ def reverse_list(head: ListNode) -> ListNode:
         prev, node = node, next_node
 
     return prev
+
+
+# 구간별 역순 연결리스트
+def reverse_list_select(head, n, m):
+    if not head or n == m:
+        return head
+
+    root = start = ListNode(0)
+    root.next = head
+
+    for i in range(m - 1):
+        start = start.next
+    end = start.next
+
+    for _ in range(m - n):
+        tmp = start.next
+        start.next = end.next
+        end.next = end.next.next
+        start.next.next = tmp
+
+    return root.next
