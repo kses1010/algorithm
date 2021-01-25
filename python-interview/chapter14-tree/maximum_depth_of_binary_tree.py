bcca12e388adf7f9e03bd29d03655b9cf03f3e8a
Sunny <thstkd92@gmail.com>
2021-01-25 14:32:49 +0900

    파이썬 알고리즘 chapter14. 이진 트리의 최대 깊이
    - BFS를 이용한 풀이법. depth가 내려갈 때마다 count 하여 풀이.

---
+++ python-interview/chapter14-tree/maximum_depth_of_binary_tree.py
@@ -0,0 +1,28 @@
+# 104. Maximum Depth of Binary Tree
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        q = deque([root])
+        depth = 0
+
+        while q:
+            depth += 1
+            # 큐 연산 추출 노드의 자식 노드 삽입
+            for _ in range(len(q)):
+                cur = q.popleft()
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+        return depth
