6a47e31cd01bb693b15a7e49ba396292e24d9415
Sunny <thstkd92@gmail.com>
2021-02-05 18:08:34 +0900

    파이썬 알고리즘 chapter19. 1비트의 개수
    - 1의 개수 계산, 비트 연산.
    - 비트 연산을 사용하여 언제든지 활용할 수 있는 알고리즘을 활용해보자.

---
+++ python-interview/chapter19-bitcontrol/number_of_1_bits.py
@@ -0,0 +1,12 @@
+# 191. Number of 1 Bits
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            n &= n - 1
+            count += 1
+        return count
+
+    def hamming_weight(self, n):
+        return bin(n ^ 0).count('1')
